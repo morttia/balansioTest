49a26d41fd6bb799b89e65642f96979bcd1edb1c
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2016-11-08 16:30:36 +0200

    Init Realm

--- app/src/main/java/com/quattrofolia/balansiosmart/MainActivity.java
+++ app/src/main/java/com/quattrofolia/balansiosmart/MainActivity.java
@@ -3,11 +3,14 @@ package com.quattrofolia.balansiosmart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.realm.Realm;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Realm.init(this); // Initialize Realm only once when the app starts. 
         setContentView(R.layout.activity_main);
     }
 }
