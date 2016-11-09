bf7fea8cbd805ea6253e0a2b09e5cce155d6afd5
mhadess <mortti.aittokoski@gmail.com>
2016-11-09 13:33:28 +0200

    Implemented SwipableCards and created own Activity for progress view that now is just a fullscreen stack of cards.

---
+++ app/src/main/java/com/quattrofolia/balansiosmart/ProgressViewActivity.java
@@ -0,0 +1,40 @@
+package com.quattrofolia.balansiosmart;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.quattrofolia.balansiosmart.cardstack.CardStack;
+
+
+public class ProgressViewActivity extends Activity {
+    private CardStack mCardStack;
+    private CardsDataAdapter mCardAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        mCardStack = (CardStack)findViewById(R.id.container);
+
+        mCardStack.setContentResource(R.layout.card_content);
+        //mCardStack.setStackMargin(20);
+
+        mCardAdapter = new CardsDataAdapter(getApplicationContext());
+        mCardAdapter.add("test1");
+        mCardAdapter.add("test2");
+        mCardAdapter.add("test3");
+        mCardAdapter.add("test4");
+        mCardAdapter.add("test5");
+
+        mCardStack.setAdapter(mCardAdapter);
+
+        if(mCardStack.getAdapter() != null) {
+            Log.i("MyActivity", "Card Stack size: " + mCardStack.getAdapter().getCount());
+        }
+    }
+
+}
